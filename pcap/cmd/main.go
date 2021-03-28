a381ca68390770bac5c36e2bb209c3638241c051
王锦涛 <jintao9@staff.weibo.com>
2021-03-28 22:14:01 +0800

    update

--- pcap/cmd/main.go
+++ pcap/cmd/main.go
@@ -1,37 +1,8 @@
 package main
 
-/**
-参考:
-https://studygolang.com/articles/14926
-https://studygolang.com/articles/11284
-*/
-import (
-	//"github.com/google/gopacket"
-	"github.com/google/gopacket/pcap"
-	"log"
-)
+import "wjt_go/pcap/pcap_base"
 
 func main() {
-
-	pcapVersion := pcap.Version()
-
-	log.Printf("pcapVersion=%+v\n", pcapVersion)
-
-	ifs,err:=pcap.FindAllDevs()
-	if err!=nil{
-
-	}
-
-	log.Printf("interfaces=%+v\n",ifs)
-
-	handle,err:=pcap.OpenLive("eth0",100000,false,-1)
-	defer handle.Close()
-
-
-	for true {
-		buf,ci,err:=handle.ReadPacketData()
-		log.Printf("ci=%+v;err=%+v\n",ci,err)
-		log.Printf("buf=%+v\n",buf)
-	}
+	pcap_base.PcapFunc()
 
 }
