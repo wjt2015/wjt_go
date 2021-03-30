1d7a49671da94b2b815f2d807b1dda48f3e0c1c0
15236286736@163.com <15236286736@163.com>
2021-03-31 02:05:53 +0800

    update

--- pcap/cmd/main.go
+++ pcap/cmd/main.go
@@ -6,6 +6,13 @@ https://studygolang.com/articles/11284
 https://blog.csdn.net/u014762921/article/details/78275428
 https://my.oschina.net/zijingshanke/blog/1031479
 https://www.devdungeon.com/content/packet-capture-injection-and-analysis-gopacket#more-on-packets
+自定义协议/解决tcp粘包问题(golang版本)
+https://studygolang.com/articles/16337
+https://my.oschina.net/zijingshanke/blog/1031479
+https://blog.csdn.net/u014762921/article/details/78275428
+
+golang实现p2p之UDP打洞
+https://blog.csdn.net/qq_31967569/article/details/82704340
 ]
  */
 import "wjt_go/pcap/pcap_base"
