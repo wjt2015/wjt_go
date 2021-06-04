d7e71273340b9406c8669d0151964651125d8168
王锦涛 <jintao9@staff.weibo.com>
2021-06-04 11:11:11 +0800

    update

--- fastdfs/http_train/http_train.go
+++ fastdfs/http_train/http_train.go
@@ -15,6 +15,12 @@ type MyHttpHandler struct{}
 func (httpHandler* MyHttpHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request){
 
 	logrus.Infof("req=%+v;resp=%+v;",req,resp)
+	logrus.Infof("cookies=%+v;header=%+v;",req.Cookies(),req.Header)
+	logrus.Infof("URL=%+v;requestURI=%+v;",req.URL,req.RequestURI)
+
+	respHeader := resp.Header()
+	respHeader.Add("golang","区块链,中间件")
+	respHeader.Add("java","企业应用后端")
 
 	resp.Write([]byte("golang http 测试!"))
 }
---
+++ fastdfs/http_train/readme.md
@@ -0,0 +1,10 @@
+
+---  
+
+测试:
+
+curl -I --cookie 'a=b;c=d;e=f' --header 'Content-Type:image' 'http://localhost:60000/single_chat/meet.json'
+
+
+---  
+
