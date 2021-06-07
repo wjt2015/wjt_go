cebe9029c83203cbd4ceea41e9ecd6a0018081c5
王锦涛 <jintao9@staff.weibo.com>
2021-06-07 19:44:47 +0800

    context.Context应用案例

--- fastdfs/http_train/http_train.go
+++ fastdfs/http_train/http_train.go
@@ -7,7 +7,14 @@ import (
 	"net/http"
 	"time"
 )
-
+/**
+参考:
+context.Context:
+https://www.cnblogs.com/-lee/p/12820994.html
+https://www.cnblogs.com/sunlong88/p/11272559.html
+https://studygolang.com/articles/17082
+https://zhuanlan.zhihu.com/p/110085652
+ */
 func init()  {
 	logrus.SetReportCaller(true)
 }
