e532737dcf4fbbc7de7992e9db7773c898bc9e25
王锦涛 <jintao9@staff.weibo.com>
2021-06-08 23:33:39 +0800

    update

--- fastdfs/tusd_train/tusd_main.go
+++ fastdfs/tusd_train/tusd_main.go
@@ -4,35 +4,33 @@ import (
 	"context"
 	"fmt"
 	"github.com/sirupsen/logrus"
+	"github.com/tus/tusd/pkg/filestore"
+	tusd "github.com/tus/tusd/pkg/handler"
 	"net/http"
 	"reflect"
 	"strings"
-
-	"github.com/tus/tusd/pkg/filestore"
-	tusd "github.com/tus/tusd/pkg/handler"
 )
 
-func main()  {
+func main() {
 	//serv()
 	//strip()
-	filestoreB()
+	//filestoreB()
+	composerFunc()
 }
 
+func strip() {
+	s1 := "ABCCD"
+	s2 := "ABCCDEF"
+	prefix := strings.TrimPrefix(s2, s1)
 
-func strip(){
-	s1:="ABCCD"
-	s2:="ABCCDEF"
-	prefix:= strings.TrimPrefix(s2, s1)
-
-	logrus.Infof("prefix=%s",prefix)
-	stripPrefixHandler:= http.StripPrefix("/bytedance/", nil)
-	logrus.Infof("stripPrefixHandler=%+v;",stripPrefixHandler)
+	logrus.Infof("prefix=%s", prefix)
+	stripPrefixHandler := http.StripPrefix("/bytedance/", nil)
+	logrus.Infof("stripPrefixHandler=%+v;", stripPrefixHandler)
 }
 
-func init()  {
+func init() {
 	logrus.SetReportCaller(true)
 }
-
 
 func serv() {
 	// Create a new FileStore instance which is responsible for
@@ -63,7 +61,7 @@ func serv() {
 		panic(fmt.Errorf("Unable to create handler: %s", err))
 	}
 
-	logrus.Infof("handlerType=%+v",reflect.ValueOf(handler))
+	logrus.Infof("handlerType=%+v", reflect.ValueOf(handler))
 
 	// Start another goroutine for receiving events from the handler whenever
 	// an upload is completed. The event will contains details about the upload
@@ -80,13 +78,13 @@ func serv() {
 	// http://localhost:8080/files
 	h := http.StripPrefix("/files/", handler)
 
-	logrus.Infof("h.type=%+v",reflect.ValueOf(h).Type())
+	logrus.Infof("h.type=%+v", reflect.ValueOf(h).Type())
 
-	http.Handle("/files/",h )
+	http.Handle("/files/", h)
 
-	port:=8090
-	logrus.Infof("http server listen on port=%d",port)
-	err = http.ListenAndServe(fmt.Sprintf(":%d",port), nil)
+	port := 8090
+	logrus.Infof("http server listen on port=%d", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		panic(fmt.Errorf("Unable to listen: %s", err))
 	}
@@ -94,15 +92,14 @@ func serv() {
 	logrus.Infof("tusd_main finish!")
 }
 
-
-func filestoreB(){
+func filestoreB() {
 	//参考filestore.go,filestore_test.go,测试学习;
-	dir:="./data/tusd_upload"
-	store:=filestore.FileStore{
+	dir := "./data/tusd_upload"
+	store := filestore.FileStore{
 		Path: dir,
 	}
-	logrus.Infof("store=%+v",store)
-	ctx:=context.Background()
+	logrus.Infof("store=%+v", store)
+	ctx := context.Background()
 	upload, err := store.NewUpload(ctx, tusd.FileInfo{
 		Size: 42,
 		MetaData: map[string]string{
@@ -110,16 +107,16 @@ func filestoreB(){
 		},
 	})
 
-	logrus.Infof("upload=%+v;err=%+v;",upload,err)
+	logrus.Infof("upload=%+v;err=%+v;", upload, err)
 
 	info, err := upload.GetInfo(ctx)
-	logrus.Infof("info=%+v;err=%+v;",info,err)
+	logrus.Infof("info=%+v;err=%+v;", info, err)
 
 	writeChunk, err := upload.WriteChunk(ctx, 0, strings.NewReader("hello world"))
-	logrus.Infof("writeChunk=%+v;err=%+v;upload=%+v;",writeChunk,err,upload)
+	logrus.Infof("writeChunk=%+v;err=%+v;upload=%+v;", writeChunk, err, upload)
 
-	info,err=upload.GetInfo(ctx)
-	logrus.Infof("new_info=%+v;err=%+v;",info,err)
+	info, err = upload.GetInfo(ctx)
+	logrus.Infof("new_info=%+v;err=%+v;", info, err)
 
 	//http.HandleFunc()
 	//func (mux *ServeMux) Handle(pattern string, handler Handler)
@@ -129,32 +126,36 @@ func filestoreB(){
 	//func (mux *ServeMux) Handle(pattern string, handler Handler)
 	//func Handle(pattern string, handler Handler) { DefaultServeMux.Handle(pattern, handler) }
 	/*
-	reader, err := upload.GetReader(ctx)
-	content, err := ioutil.ReadAll(reader)
-	logrus.Infof("context=%+v;err=%+v;",string(content),err)
-	if rc,ok:=reader.(io.Closer);ok{
-		rc.Close()
-	}
-	info,err=upload.GetInfo(ctx)
-	logrus.Infof("new2_info=%+v;err=%+v;",info,err)
-	newUpload, err := store.GetUpload(ctx, info.ID)
-	logrus.Infof("newUpload=%+v;err=%+v;",newUpload,err)
+		reader, err := upload.GetReader(ctx)
+		content, err := ioutil.ReadAll(reader)
+		logrus.Infof("context=%+v;err=%+v;",string(content),err)
+		if rc,ok:=reader.(io.Closer);ok{
+			rc.Close()
+		}
+		info,err=upload.GetInfo(ctx)
+		logrus.Infof("new2_info=%+v;err=%+v;",info,err)
+		newUpload, err := store.GetUpload(ctx, info.ID)
+		logrus.Infof("newUpload=%+v;err=%+v;",newUpload,err)
 	*/
 }
 
-func composerHandler(){
+func composerFunc() {
 
-	composer:= tusd.NewStoreComposer()
-	dir:="./data/tusd_upload"
-	store:= filestore.FileStore{Path: dir}
+	composer := tusd.NewStoreComposer()
+	dir := "./data/tusd_upload"
+	basePath := "/files/"
+	store := filestore.FileStore{Path: dir}
 	store.UseIn(composer)
 	handler, err := tusd.NewHandler(tusd.Config{
 		StoreComposer: composer,
-		BasePath:      dir,
+		BasePath:      basePath,
 	})
-	logrus.Infof("handler=%+v;err=%+v;",handler,err)
+	logrus.Infof("handler=%+v;err=%+v;", handler, err)
 
-	http.Handle("/abc/",http.StripPrefix("/abc/",handler))
+	http.Handle(basePath, http.StripPrefix(basePath, handler))
+	port := 8090
+	logrus.Infof("http server listen on port=%d;", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	logrus.Infof("http server finish!")
 
 }
-
