03cf8a7d1d612e6d64285ddc34ef5e487cd4153d
王锦涛 <jintao9@staff.weibo.com>
2021-06-19 17:30:26 +0800

    禁止编译优化命令

--- fastdfs/http_train/http_train.go
+++ fastdfs/http_train/http_train.go
@@ -3,14 +3,7 @@ package main
 import (
 	"github.com/sirupsen/logrus"
 )
-/**
-参考:
-context.Context:
-https://www.cnblogs.com/-lee/p/12820994.html
-https://www.cnblogs.com/sunlong88/p/11272559.html
-https://studygolang.com/articles/17082
-https://zhuanlan.zhihu.com/p/110085652
- */
+
 func init()  {
 	logrus.SetReportCaller(true)
 }
---
+++ fastdfs/tusd_train/readme.md
@@ -0,0 +1,6 @@
+
+```
+禁止编译优化:
+
+go build -gcflags="-N -l" -o bin/tusd_main ./fastdfs/tusd_train/
+```
